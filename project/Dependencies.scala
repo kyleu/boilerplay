883ef297d6f72ee227cbe6e4363bfa34ac14c157
Kyle Unverferth <kyle@kyleu.com>
2017-01-25 00:03:55 -0700

    Upgrade to Play 2.5.12.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   }
 
   object Play {
-    private[this] val version = "2.5.11"
+    private[this] val version = "2.5.12"
     val playLib = "com.typesafe.play" %% "play" % version
     val playFilters = play.sbt.PlayImport.filters
     val playWs = play.sbt.PlayImport.ws
