269c45e7ea603d08803e943786b7f595ae8311d9
Kyle Unverferth <kyle@kyleu.com>
2017-08-08 19:08:09 -0600

    Styling fixes.

--- app/models/settings/SettingKey.scala
+++ app/models/settings/SettingKey.scala
@@ -2,20 +2,16 @@ package models.settings
 
 import enumeratum._
 
-sealed abstract class SettingKey(val id: String, val title: String, val description: String, val default: String) extends EnumEntry {
-  override def toString = id
-}
+sealed abstract class SettingKey(val title: String, val description: String, val default: String) extends EnumEntry
 
 object SettingKey extends Enum[SettingKey] {
   case object AllowRegistration extends SettingKey(
-    id = "allow-registration",
     title = "Allow Registration",
     description = "Determines if users are allowed to sign themselves up for the system.",
     default = "true"
   )
 
   case object DefaultNewUserRole extends SettingKey(
-    id = "default-new-user-role",
     title = "Default New User Role",
     description = "Determines the role to assign newly-registered users.",
     default = "admin"
