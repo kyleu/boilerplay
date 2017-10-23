bbb00e8973003b588c9fb64291619c63fe0f05a3
Kyle Unverferth <kyle@kyleu.com>
2017-10-23 15:44:58 -0600

    Audit view improvements.

--- shared/src/main/scala/models/audit/AuditField.scala
+++ shared/src/main/scala/models/audit/AuditField.scala
@@ -8,4 +8,6 @@ object AuditField {
   implicit val jsonDecoder: Decoder[AuditField] = deriveDecoder
 }
 
-case class AuditField(k: String, o: Option[String], n: Option[String])
+case class AuditField(k: String, o: Option[String], n: Option[String]) {
+  override def toString = s"$k: ${o.getOrElse(util.NullUtils.str)} -> ${n.getOrElse(util.NullUtils.str)}"
+}
