313c4ec58cf80bb8fd396653c93f6a9802b5c409
Kyle Unverferth <kyle@kyleu.com>
2017-11-03 22:56:34 -0600

    BooPickle serialization, websocket hotness.

--- client/src/main/scala/services/socket/SocketConnection.scala
+++ client/src/main/scala/services/socket/SocketConnection.scala
@@ -8,7 +8,7 @@ import util.{BinarySerializers, JsonSerializers}
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 @JSExportTopLevel("SocketConnection")
-class SocketConnection(path: String, binary: Boolean = false) {
+class SocketConnection(val path: String, val binary: Boolean = false) {
   private[this] val eventHandler = path match {
     case "/connect" => new EventHandler {}
     case "/admin/audit/activity/connect" => new AuditEventHandler
@@ -20,13 +20,14 @@ class SocketConnection(path: String, binary: Boolean = false) {
   InitService.initIfNeeded()
   NetworkMessage.register(sendMessage)
   Logging.debug(util.Config.projectName + " is connecting...")
-  connect(path)
+  connect()
 
-  protected def connect(path: String) = {
+  protected def connect() = {
     val loc = org.scalajs.dom.document.location
     val wsProtocol = if (loc.protocol == "https:") { "wss" } else { "ws" }
     val socketUrl = s"$wsProtocol://${loc.host}$path"
-    socket.open(socketUrl)
+    val queryString = if (binary) { "?binary=true" } else { "" }
+    socket.open(socketUrl + queryString)
   }
 
   def sendMessage(rm: RequestMessage): Unit = {
