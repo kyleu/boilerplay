9f0da19fc21706a47a7fed82e6985cfdeaa08373
Kyle Unverferth <kyle@kyleu.com>
2017-11-07 11:14:27 -0700

    Ping flood on index page for testing serialization.

--- client/src/main/scala/services/socket/TestSocketConnection.scala
+++ client/src/main/scala/services/socket/TestSocketConnection.scala
@@ -2,9 +2,17 @@ package services.socket
 
 import services.event.LoggingEventHandler
 
-import scala.scalajs.js.annotation.JSExportTopLevel
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
 @JSExportTopLevel("TestSocketConnection")
 class TestSocketConnection(binary: Boolean = true, debug: Boolean = false) extends SocketConnection("test", new LoggingEventHandler, binary, debug) {
   connect("/connect")
+
+  @JSExport
+  def flood(): Unit = {
+    val ts = util.DateUtils.now
+    import scala.scalajs.js.timers.setTimeout
+    (0 until 10).foreach(_ => NetworkMessage.sendMessage(models.Ping(ts)))
+    setTimeout(100)(flood())
+  }
 }
