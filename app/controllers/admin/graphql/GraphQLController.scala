0a2fc1050b2adb2cfa9bc22042cf36aafc669d6a
Kyle Unverferth <kyle@kyleu.com>
2018-03-19 22:00:32 -0600

    Restoring GraphQL variables, closes #32

--- app/controllers/admin/graphql/GraphQLController.scala
+++ app/controllers/admin/graphql/GraphQLController.scala
@@ -38,7 +38,7 @@ class GraphQLController @javax.inject.Inject() (override val app: Application, g
     if (allowed) {
       val body = jsonObject(jsonBody(request.body)).filter(x => x._1 != "variables").toMap
       val query = body("query").as[String].getOrElse("{}")
-      val variables = body.get("variables").map(x => graphQLService.parseVariables(x.asString.getOrElse("{}")))
+      val variables = body.get("variables").map(x => graphQLService.parseVariables(x.toString))
       val operation = body.get("operationName").flatMap(_.asString)
 
       executeQuery(query, variables, operation, Credentials.fromInsecureRequest(request), app.config.debug)
