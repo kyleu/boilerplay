7218059cb86c8504229f854d3d35cf4cf3652b13
Kyle Unverferth <kyle@kyleu.com>
2018-11-21 11:07:29 -0700

    Setting fix

--- app/models/settings/Setting.scala
+++ app/models/settings/Setting.scala
@@ -1,5 +1,6 @@
 package models.settings
 
+import models.result.data.{DataField, DataFieldModel}
 import util.JsonSerializers._
 
 object Setting {
@@ -7,7 +8,11 @@ object Setting {
   implicit val jsonDecoder: Decoder[Setting] = deriveDecoder
 }
 
-final case class Setting(k: SettingKey, v: String) {
+final case class Setting(k: SettingKey, v: String) extends DataFieldModel {
+  override def toDataFields = Seq(
+    DataField("k", Some(k.toString)),
+    DataField("v", Some(v))
+  )
   lazy val isDefault = v == k.default
   override def toString = s"$k=$v"
   lazy val asBool = v == "true"
